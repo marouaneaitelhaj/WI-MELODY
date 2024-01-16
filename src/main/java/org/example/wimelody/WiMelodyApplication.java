7a9d73dc43412efba56e6b38f0820652e21c5a32
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-01-16 10:01:51 +0100

    Add initial project files

---
+++ src/main/java/org/example/wimelody/WiMelodyApplication.java
@@ -0,0 +1,13 @@
+package org.example.wimelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiMelodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiMelodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/wimelody/WiMelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wimelody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiMelodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
