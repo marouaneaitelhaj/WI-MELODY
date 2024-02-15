354afdba8f688ab21b0c7e24cb5a1b4307ecc7e4
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-02-15 15:02:07 +0100

    Add CRUD operations for Fish entity in TierController

--- src/main/java/org/example/wimelody/controllers/TierController.java
+++ src/main/java/org/example/wimelody/controllers/TierController.java
@@ -1,2 +1,49 @@
-package org.example.wimelody.controllers;public class TierController {
+package org.example.wimelody.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/fish")
+@RequiredArgsConstructor
+public class TierController {
+    private final FishService fishServiceImpl;
+
+    @PostMapping
+    public ResponseEntity<?> createFish(@Valid @RequestBody FishDtoReq fishDtoReq) {
+        fishDtoReq = fishServiceImpl.save(fishDtoReq);
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", fishDtoReq);
+        response.put("message", "Fish created successfully");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getFishs() {
+        return ResponseEntity.ok(fishServiceImpl.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getFish(@PathVariable String id) {
+        return ResponseEntity.ok(fishServiceImpl.findOne(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateFish(@PathVariable String id, @Valid @RequestBody FishDtoReq fishDtoReq) {
+        fishDtoReq = fishServiceImpl.update(fishDtoReq, id);
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", fishDtoReq);
+        response.put("message", "Fish updated successfully");
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteFish(@PathVariable String id) {
+        fishServiceImpl.delete(id);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Fish deleted successfully");
+        return ResponseEntity.ok(response);
+    }
 }
