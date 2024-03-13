4d7c7c9251ee7958c18c5879e7d70172cd6f15d0
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-03-13 16:05:35 +0000

    Add TierDtoReqWithSubscribed class

---
+++ src/main/java/org/example/wimelody/dto/tier/TierDtoReqWithSubscribed.java
@@ -0,0 +1,19 @@
+package org.example.wimelody.dto.tier;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class TierDtoReq {
+    private UUID id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+    @NotNull
+    private Double price;
+    @NotNull
+    private UUID artist_id;
+}
